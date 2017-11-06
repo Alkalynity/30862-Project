39072a7a551c69853e17b5e94ba59e138058a1f3
Alkalynity <lhack@purdue.edu>
2017-11-06 13:04:34 -0500

    Update Container.h

--- Container.h
+++ Container.h
@@ -1,13 +1,13 @@
 #include "Base.h"
 #include "Item.h"
+#include<vector>
 #ifndef CONTAINER_H_
 #define CONTAINER_H_
 
 class Container : public Base {
-
-	Item* accepts;
-	Item* items;
-
+public:
+	std::vector<std::string>accepts;
+	std::vector<std::string>items;
 };
 
 #endif
